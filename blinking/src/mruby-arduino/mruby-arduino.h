f5b3a07c614f56606eea86b62f422ed5307f418b
Matthew Nielsen <xunker@pyxidis.org>
2017-10-18 16:53:29 -0600

    Remove commented-out code

--- blinking/src/mruby-arduino/mruby-arduino.h
+++ blinking/src/mruby-arduino/mruby-arduino.h
@@ -7,20 +7,6 @@
 #include "mruby/string.h"
 #include "mruby/data.h"
 
-// #include "mruby-arduino.h"
-
 // extern "C"
 void
 mruby_arduino_init_chipKIT_or_Due(mrb_state *mrb);
-//
-//
-// void
-// mrb_mruby_arduino_gem_init(mrb_state* mrb)
-// {
-// 	mruby_arduino_init_chipKIT_or_Due(mrb);
-// }
-//
-// void
-// mrb_mruby_arduino_gem_final(mrb_state* mrb) {
-//
-// }
