c28bd677b171de27ec46517d91b7cd23c3bd6aaa
Matthew Nielsen <xunker@pyxidis.org>
2017-10-18 16:49:41 -0600

    Added comments and citations, cleaned up un-needed code and comments.

--- blinking/blinking.ino
+++ blinking/blinking.ino
@@ -1,3 +1,10 @@
+/*
+		Example Arduino IDE file to show how to run mruby on the Arudino Due using
+		mruby 1.3.0 and Arduino IDE v1.8.3. October 2017.
+		Compiled by Matthew Nielsen (xunker@pyxidis.org), based on extensive work
+		by kyab:
+		https://github.com/kyab/mruby-arduino/blob/master/samples/blink/blink.pde
+*/
 #include "src/mruby-arduino/mruby-arduino.h"
 #include "mruby/irep.h"
 
@@ -7,16 +14,8 @@ size_t total_allocated;
 
 #include "blinking.h"
 
-// mrb_value silly_func(mrb_state *mrb, mrb_value self){
-//   Serial.println("silly_func");
-// 	digitalWrite(13, HIGH);
-// 	delay(250);
-// 	digitalWrite(13, LOW);
-// 	delay(250);
-// 	return mrb_nil_value();
-// }
-
-// custom allocator to check heap shortage.
+/* Custom allocator to check heap shortage. Originally created by kyab, copied
+   from github.com/kyab/mruby-arduino/blob/master/samples/blink/blink.pde */
 void *myallocf(mrb_state *mrb, void *p, size_t size, void *ud){
 	if (size == 0){
 		free(p);
@@ -44,7 +43,7 @@ void *myallocf(mrb_state *mrb, void *p, size_t size, void *ud){
 	return ret;
 }
 
-// report memory usage
+// Used in show_memory_usage()
 #include <malloc.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -53,8 +52,10 @@ extern char _end;
 extern "C" char *sbrk(int i);
 char *ramstart=(char *)0x20070000;
 char *ramend=(char *)0x20088000;
-// end report
+// end
 
+/* Print out the current memory usage. Credit goes to "ard_newbie":
+   https://forum.arduino.cc/index.php?topic=480973.0 */
 void show_memory_usage() {
   char *heapend=sbrk(0);
   register char * stack_ptr asm ("sp");
@@ -65,20 +66,20 @@ void show_memory_usage() {
   printf("My guess at free mem: %d\n",stack_ptr - heapend + mi.fordblks);
 }
 
-// mrb_value show_memory_usage_in_ruby(mrb_state *mrb, mrb_value self) {
-//   show_memory_usage();
-//   return mrb_nil_value();
-// }
+/* Allow show_memory_usage() to be called from inside mruby as long as the
+   appropriate lines in setup() are uncommented. */
+mrb_value show_memory_usage_in_ruby(mrb_state *mrb, mrb_value self) {
+  show_memory_usage();
+  return mrb_nil_value();
+}
 
 void setup() {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-
   Serial.begin(9600);
 
   total_allocated = 0;
 
-	//Init mruby
+	/* Init mruby. Block originally by kyab and copied from
+	   github.com/kyab/mruby-arduino/blob/master/samples/blink/blink.pde */
 	Serial.println("mrb_open()...");
 	delay(100);
 	mrb = mrb_open_allocf(myallocf, NULL);
@@ -91,19 +92,21 @@ void setup() {
 		return;
 	}
 
+	/* This adds the Arduino.* and Serial.* methods to mruby, but ONLY THOSE that
+	   have been enabled in config.h. */
   mruby_arduino_init_chipKIT_or_Due(mrb);
 
-  // Serial.println("adding Silly methods");
-	// RClass *sillyClass = mrb_define_class(mrb, "Silly", mrb->object_class);
-  // mrb_define_class_method(mrb, sillyClass, "woot", silly_func, MRB_ARGS_REQ(1));
-  // mrb_define_class_method(mrb, sillyClass, "show_memory_usage", show_memory_usage_in_ruby, MRB_ARGS_NONE());
+	/* Uncomment the two lines below to allow Info.show_memory_usage to be
+	   used in ruby. */
+	// RClass *infoClass = mrb_define_class(mrb, "Info", mrb->object_class);
+  // mrb_define_class_method(mrb, infoClass, "show_memory_usage", show_memory_usage_in_ruby, MRB_ARGS_NONE());
 
   Serial.println("Setup complete");
 	delay(250);
 
 	Serial.println("set GC");
   delay(250);
-	ai = mrb_gc_arena_save(mrb);
+	ai = mrb_gc_arena_save(mrb); // see https://github.com/mruby/mruby/blob/master/doc/guides/gc-arena-howto.md
   delay(250);
 
   show_memory_usage();
@@ -117,15 +120,13 @@ void loop() {
   delay(250);
 	mrb_load_irep(mrb, bytecode);
   delay(250);
-	Serial.println("close");
+	Serial.println("Finished executing Ruby");
   delay(250);
 	mrb_close(mrb);
   delay(250);
 
 	Serial.println("Restoring GC");
-	mrb_gc_arena_restore(mrb,ai);
+	mrb_gc_arena_restore(mrb,ai); // see https://github.com/mruby/mruby/blob/master/doc/guides/gc-arena-howto.md
 
-	Serial.println("delay");
   delay(250);
-
 }
